05505832649f842f71df2665ba2da4480ffc9e90
ovoegajivwie <62547455+ovoegajivwie@users.noreply.github.com>
2021-09-08 20:45:58 +0100

    Add files via upload

---
+++ sunnyslide.js
@@ -0,0 +1,6 @@
+const menubtn = document.querySelector(".img2")
+const menuu = document.querySelector(".menu")
+
+menubtn.addEventListener("click", function () {
+    menuu.classList.toggle("menus")
+})
